bff61dbcd363b2ee6ce345bdb6a1fe5d65b3094e
Kulpot <kulpotwahu@gmail.com>
2023-09-05 22:20:10 +0800

    Add project files.

---
+++ Alignment and Packing/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+//ref link:https://www.youtube.com/watch?v=oOf29ewSins&list=PLRwVmtr-pp07XP8UBiUJ0cyORVCmCgkdA&index=35
+// alignment and Packing -- memory management -- target: no waste
+
+class MeUglyClass
+{
+    public char meChar1;
+    public int meInt;
+    public char meChar2;
+}
+
+class MainClass
+{
+    static void Main()
+    {
+        MeUglyClass mug = new MeUglyClass();
+        mug.meChar1 = 'j';
+        mug.meInt = 25;
+        mug.meChar2 = 'k';
+    }
+}
