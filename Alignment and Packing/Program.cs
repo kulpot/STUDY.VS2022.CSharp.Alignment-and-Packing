ca8b3a32dee287c6dea770e4dc9b853e20eb7604
Kulpot <kulpotwahu@gmail.com>
2023-09-05 22:21:11 +0800

    ..

--- Alignment and Packing/Program.cs
+++ Alignment and Packing/Program.cs
@@ -2,6 +2,7 @@
 
 //ref link:https://www.youtube.com/watch?v=oOf29ewSins&list=PLRwVmtr-pp07XP8UBiUJ0cyORVCmCgkdA&index=35
 // alignment and Packing -- memory management -- target: no waste
+// 
 
 class MeUglyClass
 {
